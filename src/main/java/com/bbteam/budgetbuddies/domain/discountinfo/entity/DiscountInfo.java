c13ba8301d6577d68246649d61865cf6a07c43ff
kangseungmin <rkdtmdals999@naver.com>
2024-07-14 21:57:29 +0900

    [feat] DiscountInfo Entity 생성

---
+++ src/main/java/com/bbteam/budgetbuddies/domain/discountinfo/entity/DiscountInfo.java
@@ -0,0 +1,37 @@
+package com.bbteam.budgetbuddies.domain.discountinfo.entity;
+
+import com.bbteam.budgetbuddies.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+public class DiscountInfo extends BaseEntity {
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @ColumnDefault("0")
+    private Integer likeCount;
+
+    private Integer discountRate;
+
+    @Column(length = 1000)
+    private String siteUrl;
+
+}
