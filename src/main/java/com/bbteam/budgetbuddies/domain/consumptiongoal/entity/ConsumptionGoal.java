5f28bfc1fd491bde5bb30e6db2b6835188ce10d3
kangseungmin <rkdtmdals999@naver.com>
2024-07-14 21:57:23 +0900

    [feat] ConsumptionGoal Entity 생성

---
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/entity/ConsumptionGoal.java
@@ -0,0 +1,42 @@
+package com.bbteam.budgetbuddies.domain.consumptiongoal.entity;
+
+import com.bbteam.budgetbuddies.common.BaseEntity;
+import com.bbteam.budgetbuddies.domain.category.entity.Category;
+import com.bbteam.budgetbuddies.domain.user.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+public class ConsumptionGoal extends BaseEntity {
+
+    @Column(nullable = false)
+    @Min(value = 1, message = "0 또는 음수의 목표금액을 설정할 수 없습니다.")
+    private Long goalAmount;
+
+    @Column(nullable = false)
+    @Min(value = 1, message = "0 또는 음수의 소비금액을 설정할 수 없습니다.")
+    private Long consumeAmount;
+
+    @Column(nullable = false)
+    private LocalDate goalMonth;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
