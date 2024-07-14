7351a0a08c8655461ce65862a2004810d5d688b3
kangseungmin <rkdtmdals999@naver.com>
2024-07-14 21:56:51 +0900

    [feat] BaseEntity 생성
    id, createdAt, updatedAt, deleted 공통 사용

---
+++ src/main/java/com/bbteam/budgetbuddies/common/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.bbteam.budgetbuddies.common;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SoftDelete;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+@SoftDelete // boolean 타입의 deleted 필드가 추가
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
