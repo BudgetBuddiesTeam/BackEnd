f3d3fb8173dbfa2c9b76f4712f017ae68774367d
kangseungmin <rkdtmdals999@naver.com>
2024-07-18 20:14:24 +0900

    [feat] UserRepositoryTest 생성 (테스트용)
    - 테스트용 User Entity 1개 생성하는 테스트 코드 구현

---
+++ src/test/java/com/bbteam/budgetbuddies/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.bbteam.budgetbuddies.domain.user.repository;
+
+import com.bbteam.budgetbuddies.domain.user.entity.User;
+import com.bbteam.budgetbuddies.enums.Gender;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+    @Test
+    @DisplayName("테스트용 User Entity 1개 생성")
+    void saveUser() {
+        User user = User.builder()
+            .id(1L)
+            .phoneNumber("010-1234-1234")
+            .name("홍길동")
+            .age(25)
+            .gender(Gender.MALE)
+            .email("hong@naver.com")
+            .photoUrl("abc")
+            .consumptionPattern("TypeA")
+            .lastLoginAt(LocalDateTime.now())
+            .createdAt(LocalDateTime.now())
+            .updatedAt(LocalDateTime.now())
+            .build();
+
+        userRepository.save(user);
+    }
+
+}
