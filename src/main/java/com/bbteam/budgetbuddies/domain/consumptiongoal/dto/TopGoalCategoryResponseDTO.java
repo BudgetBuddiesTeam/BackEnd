df12c8a90fd886b80de8354c91b28620f200b736
MJJ <m3k0813@gmail.com>
2024-07-18 18:54:41 +0900

    [feat] TopGoalCategoryResponseDTO 생성

---
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/dto/TopGoalCategoryResponseDTO.java
@@ -0,0 +1,20 @@
+package com.bbteam.budgetbuddies.domain.consumptiongoal.dto;
+
+import com.bbteam.budgetbuddies.domain.category.entity.Category;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TopGoalCategoryResponseDTO {
+
+	private Category category;
+
+	private Long consumeAmount;
+
+}
