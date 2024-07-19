51ada16019a05a5f33836337e31f28f1f395f46e
wnd01jun <changjun157@naver.com>
2024-07-19 20:14:37 +0900

    [feat] CommentService 에 주석 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/comment/service/CommentService.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/comment/service/CommentService.java
@@ -10,8 +10,20 @@ public interface CommentService {
     CommentResponseDto.DiscountInfoSuccessDto saveDiscountComment(Long userId, CommentRequestDto.DiscountInfoCommentDto dto);
 
 
+    /**
+     *
+     * @param discountInfoId
+     * @return List<CommentResponseDto.DiscountInfoCommentDto>
+     * 해당 로직은 익명 구분을 위한 익명 구분 숫자도 같이 return 합니다.
+     */
     List<CommentResponseDto.DiscountInfoCommentDto> findByDiscountInfo(Long discountInfoId);
 
+    /**
+     *
+     * @param supportInfoId
+     * @return List<CommentResponseDto.SupportInfoCommentDto>
+     * 해당 로직은 익명 구분을 위한 익명 구분 숫자도 같이 return 합니다.
+     */
     List<CommentResponseDto.SupportInfoCommentDto> findBySupportInfo(Long supportInfoId);
 
 
