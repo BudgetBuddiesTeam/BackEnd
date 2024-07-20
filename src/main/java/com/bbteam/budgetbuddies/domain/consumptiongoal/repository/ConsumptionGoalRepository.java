6c61564096e3794876831d25fdc6d8bc2ef12911
MJJ <m3k0813@gmail.com>
2024-07-20 17:06:23 +0900

    [feat] ConsumptionGoalRepository 생성

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
@@ -1,10 +1,18 @@
 package com.bbteam.budgetbuddies.domain.consumptiongoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bbteam.budgetbuddies.domain.consumptiongoal.entity.ConsumptionGoal;
 
 @Repository
 public interface ConsumptionGoalRepository extends JpaRepository<ConsumptionGoal, Long> {
+
+	@Query("SELECT cg FROM ConsumptionGoal cg " +
+		"WHERE cg.category.isDefault = true " +
+		"ORDER BY cg.goalAmount DESC")
+	List<ConsumptionGoal> findTopCategoriesAndGoalAmount();
 }
