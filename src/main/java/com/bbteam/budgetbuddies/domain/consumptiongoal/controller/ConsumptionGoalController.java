d3bbcb7b9189e992ff2b46882bcfad5d89c00be2
MJJ <m3k0813@gmail.com>
2024-07-20 20:40:06 +0900

    [feat] ConsumptionGoalController swagger 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/controller/ConsumptionGoalController.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/controller/ConsumptionGoalController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bbteam.budgetbuddies.domain.consumptiongoal.dto.TopGoalCategoryResponseDTO;
 import com.bbteam.budgetbuddies.domain.consumptiongoal.service.ConsumptionGoalService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,6 +25,13 @@ public class ConsumptionGoalController {
 
 	private final ConsumptionGoalService consumptionGoalService;
 
+	@Operation(summary = "또래들이 가장 큰 계획을 세운 카테고리 조회 API", description = "특정 사용자의 소비 목표 카테고리별 소비 목표 금액을 조회하는 API입니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+	})
+	@Parameters({
+		@Parameter(name = "top", description = "가장 큰 목표를 세운 카테고리의 개수를 지정합니다. (기본값은 5입니다"),
+	})
 	@GetMapping("/consumption-goal/top-categories")
 	public ResponseEntity<?> getTopGoalCategories(@RequestParam(name = "top", defaultValue = "5") int top) {
 		List<TopGoalCategoryResponseDTO> topCategory = consumptionGoalService.getTopGoalCategories(top);
