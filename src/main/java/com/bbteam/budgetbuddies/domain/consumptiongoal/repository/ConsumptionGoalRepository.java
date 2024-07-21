583f26d94e6553a1e4ae0029a4ce53cee56a8cfc
MJJ <m3k0813@gmail.com>
2024-07-21 19:01:30 +0900

    [fix] ConsumptionGoalRepository peerAge, peerGender로 필터 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
@@ -4,15 +4,22 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bbteam.budgetbuddies.domain.consumptiongoal.entity.ConsumptionGoal;
+import com.bbteam.budgetbuddies.enums.Gender;
 
 @Repository
 public interface ConsumptionGoalRepository extends JpaRepository<ConsumptionGoal, Long> {
 
-	@Query("SELECT cg FROM ConsumptionGoal cg " +
-		"WHERE cg.category.isDefault = true " +
-		"ORDER BY cg.goalAmount DESC")
-	List<ConsumptionGoal> findTopCategoriesAndGoalAmount();
+	@Query("SELECT cg FROM ConsumptionGoal cg "
+		+ "WHERE cg.category.isDefault = true "
+		+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd "
+		+ "AND cg.user.gender = :peerGender "
+		+ "ORDER BY cg.goalAmount DESC")
+	List<ConsumptionGoal> findTopCategoriesAndGoalAmount(
+		@Param("peerAgeStart") int peerAgeStart,
+		@Param("peerAgeEnd") int peerAgeEnd,
+		@Param("peerGender") Gender peerGender);
 }
