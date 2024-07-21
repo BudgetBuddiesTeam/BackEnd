d25409379ac6b43ae9ac53d5717dcfdda44ad3ae
MJJ <m3k0813@gmail.com>
2024-07-21 18:59:08 +0900

    [fix] ConsumptionGoalController 요청 peerAge, Gender 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/controller/ConsumptionGoalController.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/controller/ConsumptionGoalController.java
@@ -25,16 +25,17 @@ public class ConsumptionGoalController {
 
 	private final ConsumptionGoalService consumptionGoalService;
 
-	@Operation(summary = "또래들이 가장 큰 계획을 세운 카테고리 조회 API", description = "특정 사용자의 소비 목표 카테고리별 소비 목표 금액을 조회하는 API입니다.")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-	})
-	@Parameters({
-		@Parameter(name = "top", description = "가장 큰 목표를 세운 카테고리의 개수를 지정합니다. (기본값은 5입니다"),
-	})
+	@Operation(summary = "또래들이 가장 큰 계획을 세운 카테고리 조회 API", description = "특정 사용자의 소비 목표 카테고리별 소비 목표 금액을 조회하는 API 입니다.")
+	@ApiResponses(value = {@ApiResponse(responseCode = "COMMON200", description = "OK, 성공")})
+	@Parameters({@Parameter(name = "top", description = "가장 큰 목표를 세운 카테고리의 개수를 지정합니다. (기본값은 5입니다")})
 	@GetMapping("/top-categories")
-	public ResponseEntity<?> getTopGoalCategories(@RequestParam(name = "top", defaultValue = "5") int top) {
-		List<TopGoalCategoryResponseDTO> topCategory = consumptionGoalService.getTopGoalCategories(top);
+	public ResponseEntity<?> getTopGoalCategories(@RequestParam(name = "top", defaultValue = "5") int top,
+		@RequestParam(name = "userId") Long userId,
+		@RequestParam(name = "peerAgeStart", defaultValue = "0") int peerAgeStart,
+		@RequestParam(name = "peerAgeEnd", defaultValue = "0") int peerAgeEnd,
+		@RequestParam(name = "peerGender", defaultValue = "none") String peerGender) {
+		List<TopGoalCategoryResponseDTO> topCategory = consumptionGoalService.getTopGoalCategories(top, userId,
+			peerAgeStart, peerAgeEnd, peerGender);
 		return ResponseEntity.ok(topCategory);
 	}
 }
