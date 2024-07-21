1b12f123bb7f61866fd19ca6f8362690095b9a1e
MJJ <m3k0813@gmail.com>
2024-07-21 19:26:14 +0900

    [fix] ConsumptionGoalService @Service 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/service/ConsumptionGoalService.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/service/ConsumptionGoalService.java
@@ -2,9 +2,13 @@ package com.bbteam.budgetbuddies.domain.consumptiongoal.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.bbteam.budgetbuddies.domain.consumptiongoal.dto.TopGoalCategoryResponseDTO;
 
+@Service
 public interface ConsumptionGoalService {
 
-	List<TopGoalCategoryResponseDTO> getTopGoalCategories(int top);
+	List<TopGoalCategoryResponseDTO> getTopGoalCategories(int top, Long userId, int peerAgeStart, int peerAgeEnd,
+		String peerGender);
 }
