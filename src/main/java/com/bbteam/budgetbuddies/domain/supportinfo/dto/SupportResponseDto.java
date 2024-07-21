ae1c39133d3942c100fe72bb3ab06ee467b0bdcf
kangseungmin <rkdtmdals999@naver.com>
2024-07-21 15:55:43 +0900

    [feat] 지원 정보 Request, ResponseDto 생성

---
+++ src/main/java/com/bbteam/budgetbuddies/domain/supportinfo/dto/SupportResponseDto.java
@@ -0,0 +1,30 @@
+package com.bbteam.budgetbuddies.domain.supportinfo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupportResponseDto {
+
+    private Long id;
+
+    private String title;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private Integer anonymousNumber;
+
+    private Integer likeCount;
+
+    private String siteUrl;
+
+}
