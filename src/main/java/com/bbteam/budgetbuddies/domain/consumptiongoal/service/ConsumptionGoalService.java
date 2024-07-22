0d7c1e8edbe7b99ba9dfe6761389d79dc5b788bc
JunRain <sa2313@tukorea.ac.kr>
2024-07-22 11:59:39 +0900

    [refactor] ConsumptioGoalService 유저 소비 목표 조회 반복되는 메서드 추출

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/service/ConsumptionGoalService.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/service/ConsumptionGoalService.java
@@ -1,12 +1,13 @@
 package com.bbteam.budgetbuddies.domain.consumptiongoal.service;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.bbteam.budgetbuddies.domain.category.entity.Category;
 import com.bbteam.budgetbuddies.domain.category.repository.CategoryRepository;
 import com.bbteam.budgetbuddies.domain.consumptiongoal.dto.ConsumptionGoalResponseDto;
 import com.bbteam.budgetbuddies.domain.consumptiongoal.dto.ConsumptionGoalResponseListDto;
@@ -20,31 +21,39 @@ public class ConsumptionGoalService {
 	private final ConsumptionGoalRepository consumptionGoalRepository;
 	private final CategoryRepository categoryRepository;
 
-	public ConsumptionGoalResponseListDto findUserConsumptionGoal(Long userId, LocalDate localDate) {
-		final LocalDate goalMonth = LocalDate.of(localDate.getYear(), localDate.getMonth(), 1);
+	public ConsumptionGoalResponseListDto findUserConsumptionGoal(Long userId, LocalDate date) {
+		LocalDate goalMonth = date.withDayOfMonth(1);
+		Map<Long, ConsumptionGoalResponseDto> goalMap = initializeGoalMap(userId, goalMonth);
 
-		Map<Long, ConsumptionGoalResponseDto> consumptionGoalResponseDtoMap = categoryRepository
-			.findUserCategoryByUserId(userId)
+		updateGoalMapWithPreviousMonth(userId, goalMonth, goalMap);
+		updateGoalMapWithCurrentMonth(userId, goalMonth, goalMap);
+
+		return new ConsumptionGoalResponseListDto(new ArrayList<>(goalMap.values()));
+	}
+
+	private Map<Long, ConsumptionGoalResponseDto> initializeGoalMap(Long userId, LocalDate goalMonth) {
+		return categoryRepository.findUserCategoryByUserId(userId)
 			.stream()
-			.map(c -> ConsumptionGoalResponseDto.initializeFromCategoryAndGoalMonth(c, goalMonth))
-			.collect(Collectors.toMap(ConsumptionGoalResponseDto::getCategoryId, c -> c));
+			.collect(Collectors.toMap(
+				Category::getId,
+				category -> ConsumptionGoalResponseDto.initializeFromCategoryAndGoalMonth(category, goalMonth)
+			));
+	}
 
-		List<ConsumptionGoalResponseDto> lastMonthConsumptionGoalList = consumptionGoalRepository
-			.findConsumptionGoalByUserIdAndGoalMonth(userId, goalMonth.minusMonths(1))
-			.stream().map(ConsumptionGoalResponseDto::of).toList();
+	private void updateGoalMapWithPreviousMonth(Long userId, LocalDate goalMonth,
+		Map<Long, ConsumptionGoalResponseDto> goalMap) {
+		updateGoalMap(userId, goalMonth.minusMonths(1), goalMap);
+	}
 
-		for (ConsumptionGoalResponseDto cgd : lastMonthConsumptionGoalList) {
-			consumptionGoalResponseDtoMap.put(cgd.getCategoryId(), cgd);
-		}
+	private void updateGoalMapWithCurrentMonth(Long userId, LocalDate goalMonth,
+		Map<Long, ConsumptionGoalResponseDto> goalMap) {
+		updateGoalMap(userId, goalMonth, goalMap);
+	}
 
-		List<ConsumptionGoalResponseDto> consumptionGoalList = consumptionGoalRepository
-			.findConsumptionGoalByUserIdAndGoalMonth(userId, goalMonth)
-			.stream().map(ConsumptionGoalResponseDto::of).toList();
-
-		for (ConsumptionGoalResponseDto cgd : consumptionGoalList) {
-			consumptionGoalResponseDtoMap.put(cgd.getCategoryId(), cgd);
-		}
-
-		return new ConsumptionGoalResponseListDto(consumptionGoalResponseDtoMap.values().stream().toList());
+	private void updateGoalMap(Long userId, LocalDate month, Map<Long, ConsumptionGoalResponseDto> goalMap) {
+		consumptionGoalRepository.findConsumptionGoalByUserIdAndGoalMonth(userId, month)
+			.stream()
+			.map(ConsumptionGoalResponseDto::of)
+			.forEach(goal -> goalMap.put(goal.getCategoryId(), goal));
 	}
 }
