1440072ba06db80c349ac91630fa8f9b067721a4
MJJ <m3k0813@gmail.com>
2024-07-23 18:04:04 +0900

    [fix] ConsumptionGoalServiceImpl 로직 수정 및 메서드 분리

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/service/ConsumptionGoalServiceImpl.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/service/ConsumptionGoalServiceImpl.java
@@ -17,69 +17,69 @@ import com.bbteam.budgetbuddies.domain.user.repository.UserRepository;
 import com.bbteam.budgetbuddies.enums.Gender;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 public class ConsumptionGoalServiceImpl implements ConsumptionGoalService {
 
 	private final ConsumptionGoalRepository consumptionGoalRepository;
-	private final TopCategoryConverter topCategoryConverter;
 	private final UserRepository userRepository;
 
-	private int peerAgeStartByUser;
-	private int peerAgeEndByUser;
+	private int peerAgeStart;
+	private int peerAgeEnd;
+	private Gender peerGender;
 
 	@Override
 	@Transactional(readOnly = true)
-	public List<TopGoalCategoryResponseDTO> getTopGoalCategories(int top, Long userId, int peerAgeStart, int peerAgeEnd,
-		String peerGender) {
+	public List<TopGoalCategoryResponseDTO> getTopGoalCategories(int top, Long userId, int peerAgeS, int peerAgeE,
+		String peerG) {
 
-		Gender gender = convertToGender(peerGender);
+		User user = findUserById(userId);
 
+		checkPeerInfo(user, peerAgeS, peerAgeE, peerG);
+
+		List<ConsumptionGoal> topGoals = consumptionGoalRepository.findTopCategoriesAndGoalAmount(top, peerAgeStart,
+			peerAgeEnd, peerGender);
+		return topGoals.stream().map(TopCategoryConverter::fromEntity).collect(Collectors.toList());
+	}
+
+	private User findUserById(Long userId) {
 		Optional<User> user = userRepository.findById(userId);
 
 		if (user.isEmpty()) {
 			throw new NoSuchElementException("유저를 찾을 수 없습니다.");
 		}
 
-		if (peerAgeStart == 0 || peerAgeEnd == 0 || gender == Gender.NONE) {
-
-			gender = user.get().getGender();
-			setAgeGroupByUser(user.get().getAge());
-
-		} else {
-			peerAgeStartByUser = peerAgeStart;
-			peerAgeEndByUser = peerAgeEnd;
-		}
-
-		List<ConsumptionGoal> topGoals = consumptionGoalRepository.findTopCategoriesAndGoalAmount(top,
-			peerAgeStartByUser, peerAgeEndByUser, gender);
-		return topGoals.stream()// 여기서 top 개수만큼 제한
-			.map(topCategoryConverter::fromEntity).collect(Collectors.toList());
+		return user.get();
 	}
 
-	private Gender convertToGender(String genderString) {
-		try {
-			return Gender.valueOf(genderString.toUpperCase());
-		} catch (IllegalArgumentException e) {
-			return Gender.NONE; // default or invalid value
+	private void checkPeerInfo(User user, int peerAgeS, int peerAgeE, String peerG) {
+
+		Gender gender = Gender.valueOf(peerG.toUpperCase());
+
+		if (peerAgeS == 0 || peerAgeE == 0 || gender == Gender.NONE) {
+			peerGender = user.getGender();
+			setAgeGroupByUser(user.getAge());
+		} else {
+			peerAgeStart = peerAgeS;
+			peerAgeEnd = peerAgeE;
+			peerGender = gender;
 		}
 	}
 
 	private void setAgeGroupByUser(int userAge) {
 		if (userAge >= 20 && userAge <= 22) {
-			peerAgeStartByUser = 20;
-			peerAgeEndByUser = 22;
+			peerAgeStart = 20;
+			peerAgeEnd = 22;
 		} else if (userAge >= 23 && userAge <= 25) {
-			peerAgeStartByUser = 23;
-			peerAgeEndByUser = 25;
+			peerAgeStart = 23;
+			peerAgeEnd = 25;
 		} else if (userAge >= 26 && userAge <= 28) {
-			peerAgeStartByUser = 26;
-			peerAgeEndByUser = 28;
+			peerAgeStart = 26;
+			peerAgeEnd = 28;
 		} else if (userAge >= 29) {
-			peerAgeStartByUser = 29;
-			peerAgeEndByUser = 100;
+			peerAgeStart = 29;
+			peerAgeEnd = 100;
 		}
 	}
 }
