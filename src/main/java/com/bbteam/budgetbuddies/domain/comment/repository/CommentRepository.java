66ee00944e4e5a304fcbf6ac832f6032953f47e3
wnd01jun <changjun157@naver.com>
2024-07-25 14:36:33 +0900

    [feat] CommentRepository 익명번호 확인용 매서드 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,9 @@
 package com.bbteam.budgetbuddies.domain.comment.repository;
 
 import com.bbteam.budgetbuddies.domain.comment.entity.Comment;
+import com.bbteam.budgetbuddies.domain.discountinfo.entity.DiscountInfo;
+import com.bbteam.budgetbuddies.domain.supportinfo.entity.SupportInfo;
+import com.bbteam.budgetbuddies.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("select c from Comment c where c.discountInfo.id = :discountInfoId" +
@@ -27,4 +31,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
             " order by c.createdAt asc")
     Page<Comment> findBySupportInfoWithPaging(@Param("supportInfoId")Long supportInfoId,
                                                Pageable pageable);
+
+    Optional<Comment> findTopByUserAndDiscountInfo(User user, DiscountInfo discountInfo);
+    Optional<Comment> findTopByUserAndSupportInfo(User user, SupportInfo supportInfo);
+
 }
