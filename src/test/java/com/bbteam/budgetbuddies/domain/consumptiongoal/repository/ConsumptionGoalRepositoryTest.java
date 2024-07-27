6aa173be863779b22b39760e623f508f79392b62
MJJ <m3k0813@gmail.com>
2024-07-27 14:29:49 +0900

    [test] 또래들이 가장 큰 목표로 세운 카테고리 조회 테스트 코드

--- src/test/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepositoryTest.java
+++ src/test/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepositoryTest.java
@@ -16,6 +16,7 @@ import com.bbteam.budgetbuddies.domain.category.repository.CategoryRepository;
 import com.bbteam.budgetbuddies.domain.consumptiongoal.entity.ConsumptionGoal;
 import com.bbteam.budgetbuddies.domain.user.entity.User;
 import com.bbteam.budgetbuddies.domain.user.repository.UserRepository;
+import com.bbteam.budgetbuddies.enums.Gender;
 
 @DisplayName("ConsumptionGoal 레포지토리 테스트의 ")
 @DataJpaTest
@@ -63,12 +64,57 @@ class ConsumptionGoalRepositoryTest {
 		setUnselectedConsumptionGoal(mainUser, goalMonth, defaultCategory);
 
 		// when
-		List<ConsumptionGoal> result = consumptionGoalRepository.findConsumptionGoalByUserIdAndGoalMonth(mainUser.getId(),
+		List<ConsumptionGoal> result = consumptionGoalRepository.findConsumptionGoalByUserIdAndGoalMonth(
+			mainUser.getId(),
 			goalMonth);
 
 		// then
 		assertThat(result).usingRecursiveComparison()
 			.isEqualTo(List.of(defaultCategoryConsumptionGoal, userCategoryConsumptionGoal));
+	}
+
+	@Test
+	@DisplayName("또래 나이와 성별 정보를 통해 GoalConsumption 조회 성공")
+	void findTopCategoriesAndGoalAmount_Success() {
+		//given
+		User mainUser = userRepository.save(
+			User.builder()
+				.email("email")
+				.age(24)
+				.name("name")
+				.gender(Gender.MALE)
+				.phoneNumber("010-1234-5678")
+				.build());
+
+		Category defaultCategory = categoryRepository.save(
+			Category.builder().name("디폴트 카테고리").user(null).isDefault(true).build());
+
+		LocalDate goalMonth = LocalDate.of(2024, 07, 01);
+
+		ConsumptionGoal defaultCategoryConsumptionGoal = consumptionGoalRepository.save(ConsumptionGoal.builder()
+			.goalAmount(1L)
+			.consumeAmount(1L)
+			.user(mainUser)
+			.goalMonth(goalMonth)
+			.category(defaultCategory)
+			.build());
+
+		// when
+		int top = 4;
+		int peerAgeStart = 23;
+		int peerAgeEnd = 25;
+		Gender peerGender = Gender.MALE;
+
+		List<ConsumptionGoal> result = consumptionGoalRepository.findTopCategoriesAndGoalAmount(
+			top, peerAgeStart, peerAgeEnd, peerGender);
+
+		// then
+		ConsumptionGoal resultGoal = result.get(0);
+		assertThat(resultGoal.getGoalAmount()).isEqualTo(1L);
+		assertThat(resultGoal.getConsumeAmount()).isEqualTo(1L);
+		assertThat(resultGoal.getUser().getAge()).isEqualTo(24);
+		assertThat(resultGoal.getCategory().getName()).isEqualTo("디폴트 카테고리");
+		assertThat(resultGoal.getUser().getGender()).isEqualTo(Gender.MALE);
 	}
 
 	private void setUnselectedConsumptionGoal(User mainUser, LocalDate goalMonth, Category defaultCategory) {
