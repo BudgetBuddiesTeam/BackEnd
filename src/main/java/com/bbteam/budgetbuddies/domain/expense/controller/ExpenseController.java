4fb98a00a0b7d2785b474c7365b9e5e25757b5f6
ryogaeng <fbgyrud828@naver.com>
2024-07-27 19:48:35 +0900

    [refactor] swagger 관련 부분 분리

--- src/main/java/com/bbteam/budgetbuddies/domain/expense/controller/ExpenseController.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/expense/controller/ExpenseController.java
@@ -2,6 +2,7 @@ package com.bbteam.budgetbuddies.domain.expense.controller;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -13,13 +14,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.bbteam.budgetbuddies.domain.expense.dto.ExpenseRequestDto;
 import com.bbteam.budgetbuddies.domain.expense.dto.ExpenseResponseDto;
 import com.bbteam.budgetbuddies.domain.expense.dto.MonthlyExpenseCompactResponseDto;
 import com.bbteam.budgetbuddies.domain.expense.service.ExpenseService;
-
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -31,7 +29,7 @@ public class ExpenseController implements ExpenseApi {
 	@Override
 	@PostMapping("/add")
 	public ResponseEntity<ExpenseResponseDto> createExpense(
-		@Parameter(description = "user_id, category_id, amount, description, expenseDate") @RequestBody ExpenseRequestDto expenseRequestDto) {
+			@Parameter(description = "user_id, category_id, amount, description, expenseDate") @RequestBody ExpenseRequestDto expenseRequestDto) {
 		ExpenseResponseDto response = expenseService.createExpense(expenseRequestDto);
 		return ResponseEntity.ok(response);
 	}
@@ -39,8 +37,8 @@ public class ExpenseController implements ExpenseApi {
 	@Override
 	@GetMapping("/{userId}")
 	public ResponseEntity<MonthlyExpenseCompactResponseDto> findExpensesForMonth(Pageable pageable,
-		@PathVariable @Param("userId") Long userId,
-		@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+																				 @PathVariable @Param("userId") Long userId,
+																				 @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
 
 		return ResponseEntity.ok(expenseService.getMonthlyExpense(pageable, userId, date));
 	}
