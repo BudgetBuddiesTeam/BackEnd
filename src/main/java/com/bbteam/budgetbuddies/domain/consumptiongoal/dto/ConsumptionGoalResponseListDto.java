cf27045ed40960e969a1a64baef608a1b6d6be71
JunRain <sa2313@tukorea.ac.kr>
2024-07-29 03:51:53 +0900

    [fix] ConsumptionGoalResponseListDto 총소비액 및 총목표액 구현

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/dto/ConsumptionGoalResponseListDto.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/dto/ConsumptionGoalResponseListDto.java
@@ -4,17 +4,18 @@ import java.time.LocalDate;
 import java.util.List;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Builder
 public class ConsumptionGoalResponseListDto {
 	private LocalDate goalMonth;
+	private Long totalGoalAmount;
+	private Long totalConsumptionAmount;
 	private List<ConsumptionGoalResponseDto> consumptionGoalList;
-
-	public ConsumptionGoalResponseListDto(LocalDate goalMonth, List<ConsumptionGoalResponseDto> consumptionGoalList) {
-		this.goalMonth = goalMonth;
-		this.consumptionGoalList = consumptionGoalList;
-	}
 }
