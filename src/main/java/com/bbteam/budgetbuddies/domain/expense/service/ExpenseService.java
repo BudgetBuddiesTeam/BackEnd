c796ec860f9cf43355660aff46f1e6d2a0d403e9
JunRain <sa2313@tukorea.ac.kr>
2024-07-29 17:44:16 +0900

    [feat] ExpenseService 월별 소비 조회 비즈니스 로직 구현

--- src/main/java/com/bbteam/budgetbuddies/domain/expense/service/ExpenseService.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/expense/service/ExpenseService.java
@@ -1,11 +1,15 @@
 package com.bbteam.budgetbuddies.domain.expense.service;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Pageable;
+
 import com.bbteam.budgetbuddies.domain.expense.dto.ExpenseRequestDto;
 import com.bbteam.budgetbuddies.domain.expense.dto.ExpenseResponseDto;
-import com.bbteam.budgetbuddies.domain.category.dto.CategoryResponseDTO;
-
-import java.util.List;
+import com.bbteam.budgetbuddies.domain.expense.dto.MonthlyExpenseCompactResponseDto;
 
 public interface ExpenseService {
-    ExpenseResponseDto createExpense(ExpenseRequestDto expenseRequestDto);
+	ExpenseResponseDto createExpense(ExpenseRequestDto expenseRequestDto);
+
+	MonthlyExpenseCompactResponseDto getMonthlyExpense(Pageable pageable, Long userId, LocalDate localDate);
 }
--- src/main/java/com/bbteam/budgetbuddies/domain/expense/service/ExpenseServiceImpl.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/expense/service/ExpenseServiceImpl.java
@@ -1,36 +1,59 @@
 package com.bbteam.budgetbuddies.domain.expense.service;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.bbteam.budgetbuddies.domain.category.entity.Category;
 import com.bbteam.budgetbuddies.domain.category.repository.CategoryRepository;
 import com.bbteam.budgetbuddies.domain.expense.converter.ExpenseConverter;
 import com.bbteam.budgetbuddies.domain.expense.dto.ExpenseRequestDto;
 import com.bbteam.budgetbuddies.domain.expense.dto.ExpenseResponseDto;
+import com.bbteam.budgetbuddies.domain.expense.dto.MonthlyExpenseCompactResponseDto;
 import com.bbteam.budgetbuddies.domain.expense.entity.Expense;
 import com.bbteam.budgetbuddies.domain.expense.repository.ExpenseRepository;
 import com.bbteam.budgetbuddies.domain.user.entity.User;
 import com.bbteam.budgetbuddies.domain.user.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ExpenseServiceImpl implements ExpenseService {
 
-    private final ExpenseRepository expenseRepository;
-    private final UserRepository userRepository;
-    private final CategoryRepository categoryRepository;
-    private final ExpenseConverter expenseConverter;
+	private final ExpenseRepository expenseRepository;
+	private final UserRepository userRepository;
+	private final CategoryRepository categoryRepository;
+	private final ExpenseConverter expenseConverter;
 
-    @Override
-    public ExpenseResponseDto createExpense(ExpenseRequestDto expenseRequestDto) {
-        User user = userRepository.findById(expenseRequestDto.getUserId())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
-        Category category = categoryRepository.findById(expenseRequestDto.getCategoryId())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid category ID"));
+	@Override
+	public ExpenseResponseDto createExpense(ExpenseRequestDto expenseRequestDto) {
+		User user = userRepository.findById(expenseRequestDto.getUserId())
+			.orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
+		Category category = categoryRepository.findById(expenseRequestDto.getCategoryId())
+			.orElseThrow(() -> new IllegalArgumentException("Invalid category ID"));
 
-        Expense expense = expenseConverter.toExpenseEntity(expenseRequestDto, user, category);
-        expenseRepository.save(expense);
+		Expense expense = expenseConverter.toExpenseEntity(expenseRequestDto, user, category);
+		expenseRepository.save(expense);
 
-        return expenseConverter.toExpenseResponseDto(expense);
-    }
+		return expenseConverter.toExpenseResponseDto(expense);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public MonthlyExpenseCompactResponseDto getMonthlyExpense(Pageable pageable, Long userId, LocalDate localDate) {
+		LocalDate startOfMonth = localDate.withDayOfMonth(1);
+		LocalDate endOfMonth = localDate.withDayOfMonth(startOfMonth.lengthOfMonth());
+
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
+
+		Slice<Expense> expenseSlice = expenseRepository.findAllByUserIdForPeriod(pageable, user,
+			startOfMonth.atStartOfDay(), endOfMonth.atStartOfDay());
+
+		return expenseConverter.toMonthlyExpenseCompactResponseDto(expenseSlice, startOfMonth);
+	}
 }
