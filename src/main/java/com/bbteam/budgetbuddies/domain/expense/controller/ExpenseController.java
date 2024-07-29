354cb336a3010a7812b36e4ace9806709c33ed1c
JunRain <sa2313@tukorea.ac.kr>
2024-07-29 17:51:41 +0900

    [feat] ExpenseController Swagger 관련 코드 분리

--- src/main/java/com/bbteam/budgetbuddies/domain/expense/controller/ExpenseController.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/expense/controller/ExpenseController.java
@@ -19,39 +19,26 @@ import com.bbteam.budgetbuddies.domain.expense.dto.ExpenseResponseDto;
 import com.bbteam.budgetbuddies.domain.expense.dto.MonthlyExpenseCompactResponseDto;
 import com.bbteam.budgetbuddies.domain.expense.service.ExpenseService;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/expenses")
-public class ExpenseController {
-
+public class ExpenseController implements ExpenseApi {
 	private final ExpenseService expenseService;
 
-	@Operation(summary = "소비 내역 추가", description = "사용자가 소비 내역을 추가합니다.")
-	@ApiResponses({
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "access 토큰 만료", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "access 토큰 모양이 이상함", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
-	})
+	@Override
 	@PostMapping("/add")
 	public ResponseEntity<ExpenseResponseDto> createExpense(
-		@Parameter(description = "user_id, category_id, amount, description, expenseDate")
-		@RequestBody ExpenseRequestDto expenseRequestDto) {
+		@Parameter(description = "user_id, category_id, amount, description, expenseDate") @RequestBody ExpenseRequestDto expenseRequestDto) {
 		ExpenseResponseDto response = expenseService.createExpense(expenseRequestDto);
 		return ResponseEntity.ok(response);
 	}
 
+	@Override
 	@GetMapping("/{userId}")
-	public ResponseEntity<MonthlyExpenseCompactResponseDto> findExpensesForMonth(
-		Pageable pageable,
+	public ResponseEntity<MonthlyExpenseCompactResponseDto> findExpensesForMonth(Pageable pageable,
 		@PathVariable @Param("userId") Long userId,
 		@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
 
