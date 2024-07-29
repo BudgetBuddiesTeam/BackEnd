3905be18bf1382d2885230be415e198cdd735be1
JunRain <sa2313@tukorea.ac.kr>
2024-07-29 17:42:42 +0900

    [feat] ExpenseRepository Slice를 통해 일정 기간 소비 조회 테스트 구현

---
+++ src/test/java/com/bbteam/budgetbuddies/domain/expense/repository/ExpenseRepositoryTest.java
@@ -0,0 +1,92 @@
+package com.bbteam.budgetbuddies.domain.expense.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+import com.bbteam.budgetbuddies.domain.category.entity.Category;
+import com.bbteam.budgetbuddies.domain.category.repository.CategoryRepository;
+import com.bbteam.budgetbuddies.domain.expense.entity.Expense;
+import com.bbteam.budgetbuddies.domain.user.entity.User;
+import com.bbteam.budgetbuddies.domain.user.repository.UserRepository;
+
+@DisplayName("Expense 레포지토리 테스트의 ")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ExpenseRepositoryTest {
+	@Autowired
+	private ExpenseRepository expenseRepository;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Test
+	@DisplayName("findAllByUserIdFOrPeriod 성공")
+	void findAllByUserIdForPeriod_Success() {
+		// given
+		User user = userRepository.save(
+			User.builder().email("email").age(24).name("name").phoneNumber("010-1234-5678").build());
+
+		Category userCategory = categoryRepository.save(
+			Category.builder().name("유저 카테고리").user(user).isDefault(false).build());
+
+		LocalDate startDate = LocalDate.of(2024, 07, 01);
+		LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
+
+		List<Expense> expected = setExpense(user, userCategory, startDate);
+
+		Pageable pageable = PageRequest.of(0, 5);
+
+		Slice<Expense> result = expenseRepository.findAllByUserIdForPeriod(pageable, user,
+			startDate.atStartOfDay(), endDate.atStartOfDay());
+
+		assertThat(result.getContent()).usingRecursiveComparison().isEqualTo(expected);
+	}
+
+	private List<Expense> setExpense(User user, Category userCategory, LocalDate startDate) {
+		setUnexpectedExpense(user, userCategory, startDate);
+
+		return setExpectedExpenseOrderByDateDesc(user, userCategory, startDate);
+	}
+
+	private List<Expense> setExpectedExpenseOrderByDateDesc(User user, Category userCategory, LocalDate startDate) {
+		List<Expense> expenses = new ArrayList<>();
+
+		for (int i = startDate.lengthOfMonth(); i > startDate.lengthOfMonth() - 5; i--) {
+			Expense expense = Expense.builder()
+				.user(user)
+				.category(userCategory)
+				.amount(100000L * i)
+				.expenseDate(startDate.withDayOfMonth(i).atStartOfDay())
+				.build();
+
+			expenses.add(expenseRepository.save(expense));
+		}
+		return expenses;
+	}
+
+	private void setUnexpectedExpense(User user, Category userCategory, LocalDate startDate) {
+		for (int i = 1; i <= 5; i++) {
+			Expense expense = Expense.builder()
+				.user(user)
+				.category(userCategory)
+				.amount(100000L * i)
+				.expenseDate(startDate.withMonth(8).atStartOfDay())
+				.build();
+
+			expenseRepository.save(expense);
+		}
+	}
+}
