3eb685ccf19a7a567725bb4105fd54d5e9a77e25
wnd01jun <changjun157@naver.com>
2024-07-31 00:55:50 +0900

    [refactor] Controller 이관에 따른 annotation 비활성화

--- src/main/java/com/bbteam/budgetbuddies/domain/comment/controller/CommentController.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/comment/controller/CommentController.java
@@ -2,7 +2,7 @@ package com.bbteam.budgetbuddies.domain.comment.controller;
 
 import com.bbteam.budgetbuddies.domain.comment.dto.CommentRequestDto;
 import com.bbteam.budgetbuddies.domain.comment.dto.CommentResponseDto;
-import com.bbteam.budgetbuddies.domain.comment.service.CommentService;
+import com.bbteam.budgetbuddies.domain.comment.service.before.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,17 +10,17 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+//@RestController
 @RequiredArgsConstructor
 public class CommentController implements CommentControllerApi {
 
     private final CommentService commentService;
 
     @PostMapping("/discounts/comments")
-    public ResponseEntity<CommentResponseDto.DiscountInfoSuccessDto> saveDiscountInfoComment(
+    public ResponseEntity<CommentResponseDto.DiscountInfoCommentDto> saveDiscountInfoComment(
             @RequestParam("userId") Long userId,
             @RequestBody CommentRequestDto.DiscountInfoCommentDto discountInfoCommentDto){
-        CommentResponseDto.DiscountInfoSuccessDto dto = commentService.saveDiscountComment(userId, discountInfoCommentDto);
+        CommentResponseDto.DiscountInfoCommentDto dto = commentService.saveDiscountComment(userId, discountInfoCommentDto);
         return ResponseEntity.ok(dto);
     }
 
@@ -35,10 +35,10 @@ public class CommentController implements CommentControllerApi {
 
 
     @PostMapping("/supports/comments")
-    public ResponseEntity<CommentResponseDto.SupportInfoSuccessDto> saveSupportInfoComment(
+    public ResponseEntity<CommentResponseDto.SupportInfoCommentDto> saveSupportInfoComment(
             @RequestParam("userId") Long userId,
             @RequestBody CommentRequestDto.SupportInfoCommentDto supportInfoCommentDto){
-        CommentResponseDto.SupportInfoSuccessDto dto = commentService.saveSupportComment(userId, supportInfoCommentDto);
+        CommentResponseDto.SupportInfoCommentDto dto = commentService.saveSupportComment(userId, supportInfoCommentDto);
         return ResponseEntity.ok(dto);
     }
 
@@ -51,10 +51,12 @@ public class CommentController implements CommentControllerApi {
         return ResponseEntity.ok(result);
     }
 
-
+    @PostMapping("/comments/delete")
     public ResponseEntity<String> deleteComment(@RequestParam("commentId") Long commentId) {
         commentService.deleteComment(commentId);
         return ResponseEntity.ok("ok");
     }
+
+
 
 }
