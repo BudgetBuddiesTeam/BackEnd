befa219442866c6c57b86aa64b42c11e7afc0aef
kangseungmin <rkdtmdals999@naver.com>
2024-07-31 22:06:02 +0900

    [feat] DiscountRequest 리팩토링에 맞게 메소드 파라미터 변경 및 toEntity 메소드 오버로딩

--- src/main/java/com/bbteam/budgetbuddies/domain/discountinfo/converter/DiscountInfoConverter.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/discountinfo/converter/DiscountInfoConverter.java
@@ -1,6 +1,6 @@
 package com.bbteam.budgetbuddies.domain.discountinfo.converter;
 
-import com.bbteam.budgetbuddies.domain.discountinfo.dto.DiscountRequestDto;
+import com.bbteam.budgetbuddies.domain.discountinfo.dto.DiscountRequest;
 import com.bbteam.budgetbuddies.domain.discountinfo.dto.DiscountResponseDto;
 import com.bbteam.budgetbuddies.domain.discountinfo.entity.DiscountInfo;
 import org.springframework.stereotype.Service;
@@ -32,7 +32,26 @@ public class DiscountInfoConverter {
      * @param requestDto
      * @return entity
      */
-    public DiscountInfo toEntity(DiscountRequestDto requestDto) {
+    public DiscountInfo toEntity(DiscountRequest.RegisterDto requestDto) {
+
+        return DiscountInfo.builder()
+            .title(requestDto.getTitle())
+            .startDate(requestDto.getStartDate())
+            .endDate(requestDto.getEndDate())
+            .anonymousNumber(0)
+            .discountRate(requestDto.getDiscountRate())
+            .likeCount(0)
+            .siteUrl(requestDto.getSiteUrl())
+            .thumbnailUrl(requestDto.getThumbnailUrl())
+            .build();
+    }
+
+    /**
+     *
+     * @param requestDto
+     * @return entity
+     */
+    public DiscountInfo toEntity(DiscountRequest.UpdateDto requestDto) {
 
         return DiscountInfo.builder()
             .title(requestDto.getTitle())
