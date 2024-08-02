4a8d77d33dcce197e8bf69dd28f1b83cdef74648
MJJ <m3k0813@gmail.com>
2024-08-02 20:18:46 +0900

    [feat] 또래들이 가장 많이한 소비 top3 조회 responseDTO 생성

---
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/dto/TopConsumptionResponseDTO.java
@@ -0,0 +1,17 @@
+package com.bbteam.budgetbuddies.domain.consumptiongoal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TopConsumptionResponseDTO {
+
+	private String category;
+
+	private Long consumeAmount;
+}
