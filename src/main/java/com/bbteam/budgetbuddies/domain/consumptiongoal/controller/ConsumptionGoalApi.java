a8b094c92dbaf8254cfc8d7d28e23c91f31ef189
MJJ <m3k0813@gmail.com>
2024-08-05 15:53:23 +0900

    [fix] 또래들의 가장 큰 목표 카테고리 조회 top 수정 및 page 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/controller/ConsumptionGoalApi.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/controller/ConsumptionGoalApi.java
@@ -2,6 +2,7 @@ package com.bbteam.budgetbuddies.domain.consumptiongoal.controller;
 
 import java.time.LocalDate;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 import com.bbteam.budgetbuddies.domain.consumptiongoal.dto.ConsumptionGoalListRequestDto;
@@ -14,15 +15,25 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 public interface ConsumptionGoalApi {
-	@Operation(summary = "또래들이 가장 큰 계획을 세운 카테고리 조회 API", description = "특정 사용자의 소비 목표 카테고리별 소비 목표 금액을 조회하는 API 입니다.")
+
+	@Operation(summary = "또래들이 가장 큰 계획을 세운 카테고리 조회 Top4 API", description = "특정 사용자의 소비 목표 카테고리별 소비 목표 금액을 조회하는 API 입니다.")
 	@ApiResponses(value = {@ApiResponse(responseCode = "COMMON200", description = "OK, 성공")})
 	@Parameters({@Parameter(name = "top", description = "가장 큰 목표를 세운 카테고리의 개수를 지정합니다."),
 		@Parameter(name = "userId", description = "로그인 한 유저 아이디"),
 		@Parameter(name = "peerAgeStart", description = "또래나이 시작 범위"),
 		@Parameter(name = "peerAgeEnd", description = "또래나이 끝 범위"),
 		@Parameter(name = "peerGender", description = "또래 성별")})
-	ResponseEntity<?> getTopGoalCategories(int top, Long userId,
+	ResponseEntity<?> getTopGoalCategoriesList(int top, Long userId,
 		int peerAgeStart, int peerAgeEnd, String peerGender);
+
+	@Operation(summary = "또래들이 가장 큰 계획을 세운 카테고리 조회 API", description = "특정 사용자의 소비 목표 카테고리별 소비 목표 금액을 전체 조회하는 API 입니다.")
+	@ApiResponses(value = {@ApiResponse(responseCode = "COMMON200", description = "OK, 성공")})
+	@Parameters({@Parameter(name = "userId", description = "로그인 한 유저 아이디"),
+		@Parameter(name = "peerAgeStart", description = "또래나이 시작 범위"),
+		@Parameter(name = "peerAgeEnd", description = "또래나이 끝 범위"),
+		@Parameter(name = "peerGender", description = "또래 성별")})
+	ResponseEntity<?> getTopGoalCategoriesPage(Long userId,
+		int peerAgeStart, int peerAgeEnd, String peerGender, Pageable pageable);
 
 	@Operation(summary = "또래가 가장 큰 계획을 세운 카테고리와 이번 주 사용한 금액 조회 API", description = "로그인 한 유저의 또래 중 가장 큰 소비 목표 금액을 가진 카테고리와 이번 주 사용한 금액을 조회하는 API 입니다")
 	@ApiResponses(value = {@ApiResponse(responseCode = "COMMON200", description = "OK, 성공")})
