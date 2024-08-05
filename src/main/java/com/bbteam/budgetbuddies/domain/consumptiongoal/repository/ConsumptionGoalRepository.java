05f4abdad8f1c6fa1e46e5ede7e6321efce30195
MJJ <m3k0813@gmail.com>
2024-08-05 16:00:53 +0900

    [fix] 또래들의 가장 큰 목표 소비목표 전체 조회 Page 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,8 +22,15 @@ public interface ConsumptionGoalRepository extends JpaRepository<ConsumptionGoal
 	@Query("SELECT cg FROM ConsumptionGoal cg " + "WHERE cg.category.isDefault = true "
 		+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd " + "AND cg.user.gender = :peerGender "
 		+ "ORDER BY cg.goalAmount DESC limit :top")
-	List<ConsumptionGoal> findTopCategoriesAndGoalAmount(@Param("top") int top, @Param("peerAgeStart") int peerAgeStart,
+	List<ConsumptionGoal> findTopCategoriesAndGoalAmountLimit(@Param("top") int top,
+		@Param("peerAgeStart") int peerAgeStart,
 		@Param("peerAgeEnd") int peerAgeEnd, @Param("peerGender") Gender peerGender);
+
+	@Query("SELECT cg FROM ConsumptionGoal cg " + "WHERE cg.category.isDefault = true "
+		+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd " + "AND cg.user.gender = :peerGender "
+		+ "ORDER BY cg.goalAmount DESC")
+	Page<ConsumptionGoal> findTopCategoriesAndGoalAmount(@Param("peerAgeStart") int peerAgeStart,
+		@Param("peerAgeEnd") int peerAgeEnd, @Param("peerGender") Gender peerGender, Pageable pageable);
 
 	@Query(value = "SELECT cg FROM ConsumptionGoal AS cg WHERE cg.user.id = :userId AND cg.goalMonth = :goalMonth")
 	List<ConsumptionGoal> findConsumptionGoalByUserIdAndGoalMonth(Long userId, LocalDate goalMonth);
@@ -37,6 +46,8 @@ public interface ConsumptionGoalRepository extends JpaRepository<ConsumptionGoal
 	@Query("SELECT cg FROM ConsumptionGoal cg " + "WHERE cg.category.isDefault = true "
 		+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd " + "AND cg.user.gender = :peerGender "
 		+ "ORDER BY cg.consumeAmount DESC limit :top")
-	List<ConsumptionGoal> findTopConsumptionAndConsumeAmount(@Param("top") int top, @Param("peerAgeStart") int peerAgeStart,
+	List<ConsumptionGoal> findTopConsumptionAndConsumeAmount(@Param("top") int top,
+		@Param("peerAgeStart") int peerAgeStart,
 		@Param("peerAgeEnd") int peerAgeEnd, @Param("peerGender") Gender peerGender);
+
 }
