478ac9ad8aa62c060e1d8d91de3ddda384619c2d
MJJ <m3k0813@gmail.com>
2024-08-06 21:09:31 +0900

    [fix] 이번 달 필터 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
@@ -21,16 +21,17 @@ public interface ConsumptionGoalRepository extends JpaRepository<ConsumptionGoal
 
 	@Query("SELECT cg FROM ConsumptionGoal cg " + "WHERE cg.category.isDefault = true "
 		+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd " + "AND cg.user.gender = :peerGender "
-		+ "ORDER BY cg.goalAmount DESC limit :top")
+		+ "AND cg.goalMonth >= :currentMonth " + "ORDER BY cg.goalAmount DESC limit :top")
 	List<ConsumptionGoal> findTopCategoriesAndGoalAmountLimit(@Param("top") int top,
-		@Param("peerAgeStart") int peerAgeStart,
-		@Param("peerAgeEnd") int peerAgeEnd, @Param("peerGender") Gender peerGender);
+		@Param("peerAgeStart") int peerAgeStart, @Param("peerAgeEnd") int peerAgeEnd,
+		@Param("peerGender") Gender peerGender, LocalDate currentMonth);
 
 	@Query("SELECT cg FROM ConsumptionGoal cg " + "WHERE cg.category.isDefault = true "
 		+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd " + "AND cg.user.gender = :peerGender "
-		+ "ORDER BY cg.goalAmount DESC")
+		+ "AND cg.goalAmount >= :currentMonth " + "ORDER BY cg.goalAmount DESC")
 	Page<ConsumptionGoal> findTopCategoriesAndGoalAmount(@Param("peerAgeStart") int peerAgeStart,
-		@Param("peerAgeEnd") int peerAgeEnd, @Param("peerGender") Gender peerGender, Pageable pageable);
+		@Param("peerAgeEnd") int peerAgeEnd, @Param("peerGender") Gender peerGender, LocalDate currentMonth,
+		Pageable pageable);
 
 	@Query(value = "SELECT cg FROM ConsumptionGoal AS cg WHERE cg.user.id = :userId AND cg.goalMonth = :goalMonth")
 	List<ConsumptionGoal> findConsumptionGoalByUserIdAndGoalMonth(Long userId, LocalDate goalMonth);
@@ -45,15 +46,16 @@ public interface ConsumptionGoalRepository extends JpaRepository<ConsumptionGoal
 
 	@Query("SELECT cg FROM ConsumptionGoal cg " + "WHERE cg.category.isDefault = true "
 		+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd " + "AND cg.user.gender = :peerGender "
-		+ "ORDER BY cg.consumeAmount DESC limit :top")
+		+ "AND cg.goalMonth >= :currentMonth " + "ORDER BY cg.consumeAmount DESC limit :top")
 	List<ConsumptionGoal> findTopConsumptionAndConsumeAmountLimit(@Param("top") int top,
-		@Param("peerAgeStart") int peerAgeStart,
-		@Param("peerAgeEnd") int peerAgeEnd, @Param("peerGender") Gender peerGender);
+		@Param("peerAgeStart") int peerAgeStart, @Param("peerAgeEnd") int peerAgeEnd,
+		@Param("peerGender") Gender peerGender, LocalDate currentMonth);
 
 	@Query("SELECT cg FROM ConsumptionGoal cg " + "WHERE cg.category.isDefault = true "
 		+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd " + "AND cg.user.gender = :peerGender "
-		+ "ORDER BY cg.consumeAmount DESC")
+		+ "AND cg.goalMonth >= :currentMonth " + "ORDER BY cg.consumeAmount DESC")
 	Page<ConsumptionGoal> findTopConsumptionAndConsumeAmount(@Param("peerAgeStart") int peerAgeStart,
-		@Param("peerAgeEnd") int peerAgeEnd, @Param("peerGender") Gender peerGender, Pageable pageable);
+		@Param("peerAgeEnd") int peerAgeEnd, @Param("peerGender") Gender peerGender, LocalDate currentMonth,
+		Pageable pageable);
 
 }
