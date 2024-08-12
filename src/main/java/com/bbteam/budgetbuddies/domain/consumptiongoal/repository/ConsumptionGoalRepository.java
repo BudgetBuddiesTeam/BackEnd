3aad802b82c56605aad32aa9c4209601bd87ca1e
MJJ <m3k0813@gmail.com>
2024-08-12 15:29:51 +0900

    [refactor] 가장 많이 계획한 카테고리 및 평균 목표금액 조회 repository 구현, dto명 수정, 메서드명 수정

--- src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/repository/ConsumptionGoalRepository.java
@@ -10,8 +10,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bbteam.budgetbuddies.domain.category.entity.Category;
-import com.bbteam.budgetbuddies.domain.consumptiongoal.dto.CategoryAvgConsumptionDTO;
-import com.bbteam.budgetbuddies.domain.consumptiongoal.dto.MyConsumptionDTO;
+import com.bbteam.budgetbuddies.domain.consumptiongoal.dto.AvgConsumptionGoalDTO;
+import com.bbteam.budgetbuddies.domain.consumptiongoal.dto.MyConsumptionGoalDTO;
 import com.bbteam.budgetbuddies.domain.consumptiongoal.entity.ConsumptionGoal;
 import com.bbteam.budgetbuddies.domain.user.entity.User;
 import com.bbteam.budgetbuddies.enums.Gender;
@@ -38,19 +38,36 @@ public interface ConsumptionGoalRepository extends JpaRepository<ConsumptionGoal
 		@Param("startOfWeek") LocalDate startOfWeek, @Param("endOfWeek") LocalDate endOfWeek);
 
 	@Query(
-		"SELECT new com.bbteam.budgetbuddies.domain.consumptiongoal.dto.CategoryAvgConsumptionDTO(cg.category.id, AVG(cg.consumeAmount))"
+		"SELECT new com.bbteam.budgetbuddies.domain.consumptiongoal.dto.AvgConsumptionGoalDTO(cg.category.id, AVG(cg.consumeAmount))"
 			+ "FROM ConsumptionGoal cg " + "WHERE cg.category.isDefault = true "
 			+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd " + "AND cg.user.gender = :peerGender "
 			+ "AND cg.goalMonth >= :currentMonth " + "GROUP BY cg.category.id " + "ORDER BY AVG(cg.consumeAmount) DESC")
-	List<CategoryAvgConsumptionDTO> findAvgConsumptionByCategory(
+	List<AvgConsumptionGoalDTO> findAvgConsumptionAmountByCategory(
 		@Param("peerAgeStart") int peerAgeStart,
 		@Param("peerAgeEnd") int peerAgeEnd,
 		@Param("peerGender") Gender peerGender,
 		@Param("currentMonth") LocalDate currentMonth);
 
-	@Query("SELECT new com.bbteam.budgetbuddies.domain.consumptiongoal.dto.MyConsumptionDTO("
+	@Query("SELECT new com.bbteam.budgetbuddies.domain.consumptiongoal.dto.MyConsumptionGoalDTO("
 		+ "cg.category.id, SUM(cg.consumeAmount)) " + "FROM ConsumptionGoal cg "
 		+ "WHERE cg.category.isDefault = true " + "AND cg.user.id = :userId "
 		+ "GROUP BY cg.category.id " + "ORDER BY cg.category.id")
-	List<MyConsumptionDTO> findAllConsumptionAmountByUserId(@Param("userId") Long userId);
+	List<MyConsumptionGoalDTO> findAllConsumptionAmountByUserId(@Param("userId") Long userId);
+
+	@Query(
+		"SELECT new com.bbteam.budgetbuddies.domain.consumptiongoal.dto.AvgConsumptionGoalDTO(cg.category.id, AVG(cg.goalAmount))"
+			+ "FROM ConsumptionGoal cg " + "WHERE cg.category.isDefault = true "
+			+ "AND cg.user.age BETWEEN :peerAgeStart AND :peerAgeEnd " + "AND cg.user.gender = :peerGender "
+			+ "AND cg.goalMonth >= :currentMonth " + "GROUP BY cg.category.id " + "ORDER BY AVG(cg.goalAmount) DESC")
+	List<AvgConsumptionGoalDTO> findAvgGoalAmountByCategory(
+		@Param("peerAgeStart") int peerAgeStart,
+		@Param("peerAgeEnd") int peerAgeEnd,
+		@Param("peerGender") Gender peerGender,
+		@Param("currentMonth") LocalDate currentMonth);
+
+	@Query("SELECT new com.bbteam.budgetbuddies.domain.consumptiongoal.dto.MyConsumptionGoalDTO("
+		+ "cg.category.id, SUM(cg.goalAmount)) " + "FROM ConsumptionGoal cg "
+		+ "WHERE cg.category.isDefault = true " + "AND cg.user.id = :userId "
+		+ "GROUP BY cg.category.id " + "ORDER BY cg.category.id")
+	List<MyConsumptionGoalDTO> findAllGoalAmountByUserId(@Param("userId") Long userId);
 }
