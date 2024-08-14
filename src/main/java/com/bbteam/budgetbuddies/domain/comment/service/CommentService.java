0edeee6bf27d2de55e4f222ebb0696c642401cbc
kangseungmin <rkdtmdals999@naver.com>
2024-08-14 21:07:52 +0900

    [fix] CommentDto 수정

--- src/main/java/com/bbteam/budgetbuddies/domain/comment/service/CommentService.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/comment/service/CommentService.java
@@ -1,19 +1,12 @@
 package com.bbteam.budgetbuddies.domain.comment.service;
 
-import com.bbteam.budgetbuddies.domain.comment.converter.CommentConverter;
 import com.bbteam.budgetbuddies.domain.comment.dto.CommentRequestDto;
-import com.bbteam.budgetbuddies.domain.comment.dto.CommentResponseDto;
 import com.bbteam.budgetbuddies.domain.comment.entity.Comment;
-import com.bbteam.budgetbuddies.domain.discountinfo.entity.DiscountInfo;
-import com.bbteam.budgetbuddies.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public interface CommentService<R, T> {
 
@@ -33,7 +26,7 @@ public interface CommentService<R, T> {
 
 
 
-    T modifyComment(CommentRequestDto.CommentModifyDto dto);
+    T modifyComment(CommentRequestDto.CommentModifyRequestDto dto);
 
     Optional<Comment> findById(Long commentId);
 }
