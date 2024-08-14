2e3c112100d680db4d8284791cbe949e6d687fc7
JunRain <sa2313@tukorea.ac.kr>
2024-08-14 22:48:40 +0900

    [feat] Expense의 LocalDateTiem에 대한 요청과 응답 포맷 통

--- src/main/java/com/bbteam/budgetbuddies/domain/expense/dto/ExpenseResponseDto.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/expense/dto/ExpenseResponseDto.java
@@ -1,22 +1,26 @@
 package com.bbteam.budgetbuddies.domain.expense.dto;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExpenseResponseDto {
-    private Long expenseId;
-    private Long userId;
-    private Long categoryId;
-    private String categoryName;
-    private Long amount;
-    private String description;
-    private LocalDateTime expenseDate;
+	private Long expenseId;
+	private Long userId;
+	private Long categoryId;
+	private String categoryName;
+	private Long amount;
+	private String description;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime expenseDate;
 }
--- src/main/java/com/bbteam/budgetbuddies/domain/expense/dto/ExpenseUpdateRequestDto.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/expense/dto/ExpenseUpdateRequestDto.java
@@ -2,15 +2,19 @@ package com.bbteam.budgetbuddies.domain.expense.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ExpenseUpdateRequestDto {
 	private Long expenseId;
 	private Long categoryId;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime expenseDate;
 	private Long amount;
 }
