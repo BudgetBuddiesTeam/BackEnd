be8069a169646829af5c08094864c79bba3086b4
wnd01jun <changjun157@naver.com>
2024-09-12 14:51:43 +0900

    [refactor] NoticeRepository 불필요 method 제거

--- src/main/java/com/bbteam/budgetbuddies/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/notice/repository/NoticeRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
-    Page<Notice> findAllWithPaging(Pageable pageable);
 }
