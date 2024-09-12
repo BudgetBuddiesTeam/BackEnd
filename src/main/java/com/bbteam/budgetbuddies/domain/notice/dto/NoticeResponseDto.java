80d6c23127dd05089af4fac49934c0d6a3f08350
wnd01jun <changjun157@naver.com>
2024-09-12 16:17:32 +0900

    [refactor] updatedAt에 jsonFormat 추가

--- src/main/java/com/bbteam/budgetbuddies/domain/notice/dto/NoticeResponseDto.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/notice/dto/NoticeResponseDto.java
@@ -1,5 +1,6 @@
 package com.bbteam.budgetbuddies.domain.notice.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,5 +13,6 @@ public class NoticeResponseDto {
     private String userName;
     private String title;
     private String body;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime updatedAt;
 }
