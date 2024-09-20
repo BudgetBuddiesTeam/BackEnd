7da371137d80b12abf5afcb2c64fe0ba958b9d75
MJJ <m3k0813@gmail.com>
2024-09-20 17:11:54 +0900

    [feat] 이번 달 비교 분석 레포트 조회 dto 추가

---
+++ src/main/java/com/bbteam/budgetbuddies/domain/consumptiongoal/dto/MonthReportResponseDto.java
@@ -0,0 +1,16 @@
+package com.bbteam.budgetbuddies.domain.consumptiongoal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MonthReportResponseDto {
+
+	private String facialExpression;
+	private String comment;
+}
