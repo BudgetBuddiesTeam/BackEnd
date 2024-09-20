053c4d7a2008a2eed10a2e7085708f85a24e408d
kangseungmin <rkdtmdals999@naver.com>
2024-09-20 22:26:58 +0900

    [feat] Hashtag update 메소드 구현

--- src/main/java/com/bbteam/budgetbuddies/domain/hashtag/entity/Hashtag.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/hashtag/entity/Hashtag.java
@@ -1,6 +1,7 @@
 package com.bbteam.budgetbuddies.domain.hashtag.entity;
 
 import com.bbteam.budgetbuddies.common.BaseEntity;
+import com.bbteam.budgetbuddies.domain.hashtag.dto.HashtagRequest;
 import jakarta.persistence.Entity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -14,5 +15,9 @@ import lombok.experimental.SuperBuilder;
 public class Hashtag extends BaseEntity {
 
     private String name; // 해시태그 이름
+
+    public void update(HashtagRequest.UpdateHashtagDto updateHashtagDto) {
+        this.name = updateHashtagDto.getName();
+    }
 
 }
