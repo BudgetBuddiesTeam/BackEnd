0a7d83ddca5c708cda7ad787437256106f08651c
JunRain <sa2313@tukorea.ac.kr>
2024-10-01 14:18:29 +0900

    [feat] ReportController 신고접수를 위한 신고이력 확인

---
+++ src/main/java/com/bbteam/budgetbuddies/domain/report/controller/ReportController.java
@@ -0,0 +1,30 @@
+package com.bbteam.budgetbuddies.domain.report.controller;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bbteam.budgetbuddies.apiPayload.ApiResponse;
+import com.bbteam.budgetbuddies.domain.report.service.ReportService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/{commentId}/report")
+@RequiredArgsConstructor
+public class ReportController {
+	private final ReportService reportService;
+
+	@GetMapping("/{userId}")
+	public ApiResponse<String> isExistReport(@PathVariable @Param("commentId") Long commentId,
+		@PathVariable @Param("userId") Long userId) {
+		if (reportService.isExistReport(commentId, userId)) {
+			return ApiResponse.onSuccess("신고 이력이 존재합니다.");
+		}
+		return ApiResponse.onFailure(HttpStatus.NOT_FOUND.toString(), HttpStatus.NOT_FOUND.getReasonPhrase(),
+			"신고 이력이 존재하지 않습니다.");
+	}
+}
