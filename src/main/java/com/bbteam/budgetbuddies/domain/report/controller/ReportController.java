ab16b77f7f7be6af9586cb50cf20099fde2b356f
JunRain <sa2313@tukorea.ac.kr>
2024-10-03 16:38:03 +0900

    [feat] ReportController 댓글신고 API 구현

--- src/main/java/com/bbteam/budgetbuddies/domain/report/controller/ReportController.java
+++ src/main/java/com/bbteam/budgetbuddies/domain/report/controller/ReportController.java
@@ -4,18 +4,23 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bbteam.budgetbuddies.apiPayload.ApiResponse;
+import com.bbteam.budgetbuddies.domain.report.dto.request.ReportRequestDto;
+import com.bbteam.budgetbuddies.domain.report.dto.response.ReportResponseDto;
 import com.bbteam.budgetbuddies.domain.report.service.ReportService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/{commentId}/report")
 @RequiredArgsConstructor
-public class ReportController implements ReportApi{
+public class ReportController implements ReportApi {
 	private final ReportService reportService;
 
 	@GetMapping("/{userId}")
@@ -26,5 +31,13 @@ public class ReportController implements ReportApi{
 		}
 		return ApiResponse.onFailure(HttpStatus.NOT_FOUND.toString(), HttpStatus.NOT_FOUND.getReasonPhrase(),
 			"신고 이력이 존재하지 않습니다.");
+	}
+
+	@PostMapping("/{userId}")
+	public ApiResponse<ReportResponseDto> reportComment(@Valid @RequestBody ReportRequestDto request,
+		@PathVariable @Param("commentId") Long commentId, @PathVariable @Param("userId") Long userId) {
+
+		ReportResponseDto response = reportService.reportComment(request, commentId, userId);
+		return ApiResponse.onSuccess(response);
 	}
 }
